db0db4028bb986f5625e7c92305f29c368ce12ec
admin-nomadsconsulting-com <admin@nomadsconsulting.com>
2022-08-07 17:19:43 +0200

    Initial commit

---
+++ src-azure/DotnetFrameworkTest/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DotnetFrameworkTest.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ src-azure/DotnetFrameworkTest/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace DotnetFrameworkTest.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
