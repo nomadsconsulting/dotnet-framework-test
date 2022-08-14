ab8a6f2cae1eb740336c6e47612ee176598ad641
André Steyn <asteyn@eu.trimblecorp.net>
2022-08-14 09:42:28 +0200

    Add exception

--- DotnetFrameworkTest/Global.asax.cs
+++ DotnetFrameworkTest/Global.asax.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -18,6 +15,8 @@ namespace DotnetFrameworkTest
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            throw new Exception("Hello World");
         }
     }
 }
